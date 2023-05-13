b0893b83861ed11e68d63935b1cf96291265b9b1
Youssef El idrissi <youssefelidrissi200@gmail.com>
2023-05-13 15:53:55 +0100

    learn Variable

---
+++ swift/variable.swift
@@ -0,0 +1,39 @@
+import SwiftUI
+
+import Foundation
+
+var integer: Int = 2003
+var strings: String = "HELLO JOSEPH"
+var doubles: Double = 299.122
+var Fboolean: Bool = false
+var Tboolean: Bool = true
+
+
+var KK: Bool = false
+
+
+print("Hello there \(integer)\n")
+print("Another print \(strings) | \n")
+
+print("Boolean is \(Fboolean) , \(Tboolean), \(KK)")
+
+exit(2)
+
+// {
+// var greeting: String = "Hello, playground"
+// var i = 2003
+
+// var OPER = 3.333
+
+
+// print("HELLO \(OPER) form here")
+
+
+// let iL = 23
+
+// print(iL )
+// print(greeting)
+// print(i)
+
+// }
+
